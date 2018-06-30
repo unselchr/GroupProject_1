161d9369567b9d0250a6cf56bb223ad9c33a9dae
rspears417 <racheal.spears417@gmail.com>
2018-06-30 11:11:09 -0500

    index html done

--- assets/accountSystem.js
+++ assets/accountSystem.js
@@ -39,7 +39,7 @@ var uiConfig = {
       uiShown: function() {
         // The widget is rendered.
         // Hide the loader.
-        document.getElementById('loader').style.display = 'none';//!!!!!!!!!!!!!!!!!!!!!!!
+        // document.getElementById('loader').style.display = 'none';//!!!!!!!!!!!!!!!!!!!!!!!
       }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
